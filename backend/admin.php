fc121c255842c7bc5169df654ab745588800ccbf
device999 <sattari4@hotmail.com>
2019-05-28 00:09:11 +0200

    UPDATE:refactoring

---
+++ backend/admin.php
@@ -0,0 +1,58 @@
+<?php
+/*
+$login = $_POST["login"];
+$password = $_POST["password"];
+print "Demo";
+
+if($login=="demo" && $password =="12345"){
+    print "Success";
+}else{
+    print "Failed";
+}
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.3.0/css/flag-icon.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.3.0/css/flag-icon.min.css">
+</head>
+<body>
+<style>
+.white{
+    color:white!important; 
+}
+</style>
+
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header active">
+      <a class="navbar-brand disabled white">It-Systemen</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Statistics</a></li>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Language<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="de.php"><span class="flag-icon flag-icon-de"></span> Deutch</a></li>
+          <li><a href="en.php"><span class="flag-icon flag-icon-us"></span> English</a></li>
+          <li><a href="ru.php"><span class="flag-icon flag-icon-ru"></span> Russian</a></li>
+        </ul>
+      </li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
+    </ul>
+  </div>
+</nav>
+
+
+</body>
+</html>
