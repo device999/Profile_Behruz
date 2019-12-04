a10012a2e85fa0435925882997ba2f4f9aea3942
device999 <sattari4@hotmail.com>
2019-12-04 22:27:07 +0100

    UPDATE:added separated model

--- backend/admin.php
+++ backend/admin.php
@@ -1,15 +1,14 @@
 <?php
-/*
+
 $login = $_POST["login"];
 $password = $_POST["password"];
-print "Demo";
 
 if($login=="demo" && $password =="12345"){
     print "Success";
 }else{
     print "Failed";
 }
-*/
+
 ?>
 
 <!DOCTYPE html>
@@ -21,6 +20,7 @@ if($login=="demo" && $password =="12345"){
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <script src="./js/include.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.3.0/css/flag-icon.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.3.0/css/flag-icon.min.css">
 </head>
@@ -32,26 +32,11 @@ if($login=="demo" && $password =="12345"){
 </style>
 
 
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header active">
-      <a class="navbar-brand disabled white">It-Systemen</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Statistics</a></li>
-      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Language<span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a href="de.php"><span class="flag-icon flag-icon-de"></span> Deutch</a></li>
-          <li><a href="en.php"><span class="flag-icon flag-icon-us"></span> English</a></li>
-          <li><a href="ru.php"><span class="flag-icon flag-icon-ru"></span> Russian</a></li>
-        </ul>
-      </li>
-    </ul>
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
-    </ul>
-  </div>
-</nav>
+<div w3-include-html="header.html"></div>
+
+<script>
+includeHTML();
+</script>
 
 
 </body>
---
+++ backend/language.php
@@ -0,0 +1,204 @@
+<?php
+$language = $_GET["lan"];
+$contents = file_get_contents('./translations/'.$language.'.json');
+$obj = json_decode($contents,true);
+var_dump($obj["service"]);
+?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <title>Bootstrap Example</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+        <script src="./js/include.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.3.0/css/flag-icon.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.3.0/css/flag-icon.min.css">
+    </head>
+
+    <body>
+        <style>
+            .white {
+                color: white!important;
+            }
+        </style>
+
+<div w3-include-html="header.html"></div>
+
+<script>
+includeHTML();
+</script>
+
+            <div class="container">
+                <div class="page-header">
+                    <h1>Menu bar</h1>
+                </div>
+                <form class="form-horizontal">
+
+                    <div class="form-group">
+                        <label for="inputEmail3" class="col-sm-2 control-label">About</label>
+                        <div class="col-sm-10">
+                            <input type="text" value="<?php echo $obj["menu"]["about"]; ?>" name="about" class="form-control" id="inputEmail3" placeholder="Title name">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputPassword3" class="col-sm-2 control-label">Services</label>
+                        <div class="col-sm-10">
+                            <input type="text" value="<?php echo $obj["menu"]["services"]; ?>" name="services" class="form-control" id="inputEmail3" placeholder="Title name">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputPassword3" class="col-sm-2 control-label">Portfolio</label>
+                        <div class="col-sm-10">
+                            <input type="text" value="<?php echo $obj["menu"]["portfolio"]; ?>" name="portfolio" class="form-control" id="inputEmail3" placeholder="Title name">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputPassword3" class="col-sm-2 control-label">Contact</label>
+                        <div class="col-sm-10">
+                            <input type="text" value="<?php echo $obj["menu"]["contact"]; ?>" name="contact" class="form-control" id="inputEmail3" placeholder="Title name">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10">
+                            <button type="submit" class="btn btn-default">Save</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <hr>
+            <div class="container">
+                <div class="page-header">
+                    <h1>Header</h1>
+                </div>
+                <form class="form-horizontal">
+
+                    <div class="form-group">
+                        <label for="inputEmail3" class="col-sm-2 control-label">Title</label>
+                        <div class="col-sm-10">
+                            <input type="text" value="<?php echo $obj["header"]["title"]; ?>" name="about" class="form-control" id="inputEmail3" placeholder="Title name">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputPassword3" class="col-sm-2 control-label">Subtitle</label>
+                        <div class="col-sm-10">
+                        <textarea type="text" name="portfolio" class="form-control" id="inputEmail3" placeholder="Title name"><?php echo $obj["header"]["subtitle"];?></textarea>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputPassword3" class="col-sm-2 control-label">Button</label>
+                        <div class="col-sm-10">
+                        <input type="text" value="<?php echo $obj["header"]["button"]; ?>" name="services" class="form-control" id="inputEmail3" placeholder="Title name">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10">
+                            <button type="submit" class="btn btn-default">Save</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <hr>
+            <div class="container">
+                <div class="page-header">
+                    <h1>About</h1>
+                </div>
+                <form class="form-horizontal">
+                    <div class="form-group">
+                        <label for="inputEmail3" class="col-sm-2 control-label">Title</label>
+                        <div class="col-sm-10">
+                            <input type="text" value="<?php echo $obj["about"]["title"]; ?>" name="about" class="form-control" id="inputEmail3" placeholder="Title name">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputPassword3" class="col-sm-2 control-label">Subtitle</label>
+                        <div class="col-sm-10">
+                        <textarea type="text" name="portfolio" class="form-control" id="inputEmail3" placeholder="Title name"><?php echo $obj["about"]["subtitle"];?></textarea>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputPassword3" class="col-sm-2 control-label">Button</label>
+                        <div class="col-sm-10">
+                            <input type="text" value="<?php echo $obj["about"]["button"]; ?>" name="portfolio" class="form-control" id="inputEmail3" placeholder="Title name">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10">
+                            <button type="submit" class="btn btn-default">Save</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <hr>
+            <div class="container">
+                <div class="page-header">
+                    <h1>Service</h1>
+                </div>
+                <form class="form-horizontal">
+                    <div class="form-group">
+                        <label for="inputEmail3" class="col-sm-2 control-label">Title</label>
+                        <div class="col-sm-10">
+                            <input type="text" value="<?php echo $obj["about"]["title"]; ?>" name="about" class="form-control" id="inputEmail3" placeholder="Title name">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputPassword3" class="col-sm-2 control-label">Subtitle</label>
+                        <div class="col-sm-10">
+                        <textarea type="text" name="portfolio" class="form-control" id="inputEmail3" placeholder="Title name"><?php echo $obj["about"]["subtitle"];?></textarea>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputPassword3" class="col-sm-2 control-label">Button</label>
+                        <div class="col-sm-10">
+                            <input type="text" value="<?php echo $obj["about"]["button"]; ?>" name="portfolio" class="form-control" id="inputEmail3" placeholder="Title name">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10">
+                            <button type="submit" class="btn btn-default">Save</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <hr>
+            <div class="container">
+                <div class="page-header">
+                    <h1>Contact</h1>
+                </div>
+                <form class="form-horizontal">
+                    <div class="form-group">
+                        <label for="inputEmail3" class="col-sm-2 control-label">Title</label>
+                        <div class="col-sm-10">
+                            <input type="text" value="<?php echo $obj["contact"]["title"]; ?>" name="about" class="form-control" id="inputEmail3" placeholder="Title name">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputPassword3" class="col-sm-2 control-label">Subtitle</label>
+                        <div class="col-sm-10">
+                        <textarea type="text" name="portfolio" class="form-control" id="inputEmail3" placeholder="Title name"><?php echo $obj["contact"]["subtitle"];?></textarea>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputPassword3" class="col-sm-2 control-label">Number</label>
+                        <div class="col-sm-10">
+                        <input type="text" value="<?php echo $obj["contact"]["number"]; ?>" name="about" class="form-control" id="inputEmail3" placeholder="Title name">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputPassword3" class="col-sm-2 control-label">Email</label>
+                        <div class="col-sm-10">
+                            <input type="text" value="<?php echo $obj["contact"]["email"]; ?>" name="contact" class="form-control" id="inputEmail3" placeholder="Title name">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10">
+                            <button type="submit" class="btn btn-default">Save</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+    </body>
+
+    </html>
